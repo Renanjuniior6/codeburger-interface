d57d994f17ba493ec42146e41e1988ad9ca38bb6
Renanjuniior6 <renanjrr679@gmail.com>
2023-11-30 16:37:51 -0300

    alterado BaseUrl em api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeburger = axios.create({
-  baseURL: 'https://codeburger-backend-production-12f3.up.railway.app/'
+  baseURL: 'https://codeburger-backend-production-6629.up.railway.app/'
 })
 
 apiCodeburger.interceptors.request.use(async config => {
